11b35df5ff7b4acf62168e7ae9578fa0d7d78db0
ciaran <ciaran@sora-tech.co.uk>
2020-11-03 14:26:08 +0000

    Response object basic testing

---
+++ src/GobpherLib/Response.cs
@@ -0,0 +1,15 @@
+﻿namespace GobpherLib
+{
+    public class Response
+    {
+        public Response(string data)
+        {
+        }
+
+        public ResponseType Type { get; private set; } = ResponseType.Unknown;
+        public string Display { get; private set; } = string.Empty;
+        public string Selector { get; private set; } = string.Empty;
+        public string Domain { get; private set; } = string.Empty;
+        public int Port { get; private set; } = 70; //Default Gopher port
+    }
+}
---
+++ src/GopherLib.Test/ResponseTests/Defaults.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
+using GobpherLib;
+
+namespace GopherLib.Test.ResponseTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class Defaults
+    {
+        [Test]
+        public void Response_Empty_IsNotNull()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+        }
+
+        [Test]
+        public void Response_Empty_NoType()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(ResponseType.Unknown, response.Type);
+        }
+
+        [Test]
+        public void Response_Empty_DisplayEmpty()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+            Assert.IsEmpty(response.Display);
+        }
+
+        [Test]
+        public void Response_Empty_SelectorEmpty()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+            Assert.IsEmpty(response.Selector);
+        }
+
+        [Test]
+        public void Response_Empty_DomainEmpty()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+            Assert.IsEmpty(response.Domain);
+        }
+
+        [Test]
+        public void Response_Empty_HasPort()
+        {
+            string data = string.Empty;
+
+            var response = new Response(data);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(70, response.Port);
+        }
+    }
+}
