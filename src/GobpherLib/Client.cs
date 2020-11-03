20301c62949bba99f3be1ff3422493a72caf56c4
ciaran <ciaran@sora-tech.co.uk>
2020-11-03 16:25:10 +0000

    Client type created

---
+++ src/GobpherLib/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GobpherLib
+{
+    public class Client
+    {
+        public Client(Uri domain)
+        {
+            if(domain.Scheme != "gopher")
+            {
+                throw new Exception("Invalid scheme");
+            }
+
+            Domain = domain;
+        }
+
+        public Uri Domain { get; private set; }
+    }
+}
---
+++ src/GopherLib.Test/ClientTests/Request.cs
@@ -0,0 +1,30 @@
+﻿using GobpherLib;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GopherLib.Test.ClientTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class Request
+    {
+        [Test]
+        public void Client_Created_SetsDomain()
+        {
+            var domain = new Uri("gopher://example.com");
+
+            var client = new Client(domain);
+
+            Assert.AreEqual("example.com", client.Domain.Host);
+        }
+
+        [Test]
+        public void Client_NonGopher_Throws()
+        {
+            var domain = new Uri("http://example.com");
+
+            Assert.Throws<Exception>(() => new Client(domain));
+        }
+    }
+}
