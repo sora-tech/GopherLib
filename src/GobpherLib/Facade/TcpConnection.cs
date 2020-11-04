8a53770539aa077de8a79e8b5517fdf4a6d28bbf
ciaran <ciaran@sora-tech.co.uk>
2020-11-04 16:51:55 +0000

    TcpClient facade created

---
+++ src/GobpherLib/Facade/TcpConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace GobpherLib.Facade
+{
+    public class TcpConnection : ITcpConnection
+    {
+        private readonly TcpClient client;
+
+        public TcpConnection()
+        {
+            client = new TcpClient();
+        }
+
+        public bool Connected => client.Connected;
+
+        public void Connect(string domain, int port)
+        {
+            client.Connect(domain, port);
+        }
+
+        public Stream GetStream()
+        {
+            return client.GetStream();
+        }
+    }
+}
