376879a4bf7516a1d3728f53717a6deb90106221
ciaran <ciaran@sora-tech.co.uk>
2020-11-25 14:15:13 +0000

    Creating a Browser to manage all the behaviour for a client

---
+++ src/Gopher.Cli.Test/BrowserTests.cs
@@ -0,0 +1,120 @@
+﻿using Gopher.Cli.Facade;
+using GopherLib;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Gopher.Cli.Test
+{
+    [TestFixture]
+    [Category("Cli")]
+    [ExcludeFromCodeCoverage]
+    public class BrowserTests
+    {
+        [Test]
+        public void Browser_Setup_Uri()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            Assert.IsNotNull(browser.Uri);
+            Assert.AreEqual("gopher://localhost/", browser.Uri.AbsoluteUri);
+        }
+
+        [Test]
+        public void Browser_Request_MakesRequest()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var connection = Substitute.For<IConnection>();
+            connectionfactory.CreateSimple().Returns(connection);
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            connection.Open(Arg.Any<string>(), Arg.Any<int>()).Returns(true);
+
+            browser.Request("");
+
+            connection.Received(1).Request(Arg.Any<string>());
+        }
+
+        [Test]
+        public void Browser_NoData_DrawsEmpty()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var console = Substitute.For<IConsole>();
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            Assert.DoesNotThrow(() => browser.Draw(console));
+
+            console.Received(1).Write("server: gopher://localhost/");
+        }
+
+        [Test]
+        public void Browser_RequestData_SetsDisplay()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var connection = Substitute.For<IConnection>();
+            connectionfactory.CreateSimple().Returns(connection);
+            var console = Substitute.For<IConsole>();
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            connection.Open(Arg.Any<string>(), Arg.Any<int>()).Returns(true);
+            connection.Request(Arg.Any<string>()).Returns("0Test Display\tSelector Text\tDomain Info\t71");
+
+            browser.Request("");
+
+            browser.Draw(console);
+
+            console.Received(1).WriteLine(Arg.Is("0 Test Display : Domain Info                                                    "));
+            console.Received(1).Write("server: gopher://localhost/");
+        }
+
+        [Test]
+        public void Browser_InputEsc_ReturnsTrue()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var console = Substitute.For<IConsole>();
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            console.ReadKey().Returns(new ConsoleKeyInfo('0', ConsoleKey.Escape, false, false, false));
+
+            var result = browser.Input(console);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Browser_Inputq_ReturnsTrue()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var console = Substitute.For<IConsole>();
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            console.ReadKey().Returns(new ConsoleKeyInfo('q', ConsoleKey.Q, false, false, false));
+
+            var result = browser.Input(console);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Browser_InputOther_ReturnsFalse()
+        {
+            var connectionfactory = Substitute.For<IConnectionFactory>();
+            var console = Substitute.For<IConsole>();
+
+            var browser = new Browser(connectionfactory, "gopher://localhost");
+
+            console.ReadKey().Returns(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false));
+
+            var result = browser.Input(console);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ src/Gopher.Cli/Browser.cs
@@ -0,0 +1,119 @@
+﻿using Gopher.Cli.Facade;
+using GopherLib;
+using System;
+using System.Collections.Generic;
+
+namespace Gopher.Cli
+{
+    public class Browser
+    {
+        private readonly IConnectionFactory connectionFactory;
+        private Selector selector;
+
+        public Browser(IConnectionFactory factory, string uri)
+        {
+            Uri = new Uri(uri);
+            this.connectionFactory = factory;
+            this.selector = new Selector(new List<Response>(), this.Width);
+        }
+
+        public int Width { get; set; } = 80;
+        public Uri Uri { get; private set; }
+
+        public void Request(string selector)
+        {
+            var client = new Client(Uri);
+
+            var response = client.Menu(connectionFactory.CreateSimple(), selector);
+
+            this.selector = new Selector(response, Width);
+        }
+        public void Request(Response request)
+        {
+            if(request.Type == ItemType.Info)
+            {
+                return;
+            }
+
+            if(request.Domain != Uri.Host)
+            {
+                if(request.Domain.StartsWith("gopher://") == false)
+                {
+                    Uri = new Uri("gopher://" + request.Domain);
+                }
+                else
+                {
+                    Uri = new Uri(request.Domain);
+                }
+                
+            }
+
+            var client = new Client(Uri);
+            var response = new List<Response>();
+            switch (request.Type)
+            {
+                case ItemType.Unknown:
+                    break;
+                case ItemType.File:
+                    break;
+                case ItemType.Directory:
+                    response = client.Menu(connectionFactory.CreateSimple(), request.Selector);
+                    break;
+                case ItemType.PhoneBook:
+                    break;
+                case ItemType.Error:
+                    break;
+                case ItemType.BinHexed:
+                    break;
+                case ItemType.DOSBinary:
+                    break;
+                case ItemType.UUEncoded:
+                    break;
+                case ItemType.IndexSearch:
+                    break;
+                case ItemType.Telnet:
+                    break;
+                case ItemType.Binary:
+                    break;
+                case ItemType.RedundantServer:
+                    break;
+                case ItemType.TN3270:
+                    break;
+                case ItemType.GIF:
+                    break;
+                case ItemType.Image:
+                    break;
+                default:
+                    break;
+            }
+
+            this.selector = new Selector(response, Width);
+        }
+
+        public void Draw(IConsole console)
+        {
+            this.selector.Draw(console);
+            console.WriteLine();
+            console.Write($"server: {Uri}");
+        }
+
+        public bool Input(IConsole console)
+        {
+            var key = console.ReadKey();
+
+            if(key.Key == ConsoleKey.Escape || key.Key == ConsoleKey.Q)
+            {
+                return false;
+            }
+
+            selector.ReadKey(key);
+
+            if(key.Key == ConsoleKey.Enter)
+            {
+                Request(selector.Selected);
+            }
+
+            return true;
+        }
+    }
+}
